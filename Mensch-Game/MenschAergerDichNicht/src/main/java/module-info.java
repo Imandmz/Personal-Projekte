b327216be850d108b9889e77ff0d475a5925c2a6
Imandmz <142630858+Imandmz@users.noreply.github.com>
2025-04-24 11:17:38 +0200

    Delete Mensch-Game directory

--- Mensch-Game/MenschAergerDichNicht/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module app.menschaergerdichnicht {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens app.menschaergerdichnicht to javafx.fxml;
-    exports app.menschaergerdichnicht;
-}
